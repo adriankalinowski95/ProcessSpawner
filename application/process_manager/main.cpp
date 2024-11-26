131c54bf9548512eea4eb3d7211c89df2903745e
System Administrator <root@MacBook-Pro-Adrian.local>
2024-11-26 14:48:19 +0100

    protobuf inside application/shared

--- application/process_manager/main.cpp
+++ application/process_manager/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <communication.pb.h>
 
 int main(int argc, char** argv) {
     std::cout << "hello world!" << std::endl;
+    Communication::StopRequest stop_request;
+    if (stop_request.process_id() == "123") {
+        std::cout << "process_id is 123" << std::endl;
+    }
 
     return 0;
 }
